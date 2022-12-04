5edab89966211708cbedef00a913018e24d9d3e7
Marco <marco.rios@upeu.edu.pe>
2022-12-04 18:46:33 -0500

    Añadido ventana principal html

---
+++ src/app/pages/principal/principal.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
+import {PrincipalRoutingModule} from "./principal-routing.module";
+import {CommonModule} from "@angular/common";
+
+export let validateAndFlattenComponentImports = undefined;
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    PrincipalRoutingModule,
+    ReactiveFormsModule
+
+  ],
+  providers: []
+})
+export class principalmodule {
+
+
+}
