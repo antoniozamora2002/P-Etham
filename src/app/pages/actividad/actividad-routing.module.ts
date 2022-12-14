98a469bb264aba3eb66ed68dea5b4935d2b8a673
Marco <marco.rios@upeu.edu.pe>
2022-12-13 20:00:47 -0500

    arreglao rutas

--- src/app/pages/actividad/actividad-routing.module.ts
+++ src/app/pages/actividad/actividad-routing.module.ts
@@ -14,52 +14,57 @@ import {PersonaComponent} from "./persona/persona.component";
   import { TipoMaterialesComponent } from "./tipo-materiales/tipo-materiales.component";
   import {PersonaTallerComponent} from "./persona-taller/persona-taller.component";
   import {ReporteComponent} from "./reporte/reporte.component";
+  import { DashboardComponent } from "../dashboard/dashboard.component";
 const routes: Routes = [
+  {
+    path:'',
+    component: DashboardComponent,
+  },
   {
     path:'',
     component: ActividadComponent,
     children: [
-      {
-        path: 'materiales',
-        component: MaterialesComponent,
-      },
-      {
-        path: 'programas',
-        component: ProgramasComponent,
-      },
-      {
-        path: 'talleres',
-        component: TalleresComponent,
-      },
-      {
-        path: 'personas',
-        component: PersonaComponent,
-      },
-      {
-        path: 'facultades',
-        component: FacultadComponent,
-      },
-      {
-        path: 'tipopersonas',
-        component: TipoPersonaComponent
-      },
-      {
-        path: 'carreras',
-        component: CarreraComponent
-      },
-      {
-        path: 'tipomateriales',
-        component: TipoMaterialesComponent
-      },
-      {
-        path: 'personatalleres',
-        component: PersonaTallerComponent
-      },
-      {
-        path: 'reporte',
-        component: ReporteComponent
-      }
-    ]
+    {
+      path: 'materiales',
+      component: MaterialesComponent,
+    },
+    {
+      path: 'programas',
+      component: ProgramasComponent,
+    },
+    {
+      path: 'talleres',
+      component: TalleresComponent,
+    },
+    {
+      path: 'personas',
+      component: PersonaComponent,
+    },
+    {
+      path: 'facultades',
+      component: FacultadComponent,
+    },
+    {
+      path: 'tipopersonas',
+      component: TipoPersonaComponent
+    },
+    {
+      path: 'carreras',
+      component: CarreraComponent
+    },
+    {
+      path: 'tipomateriales',
+      component: TipoMaterialesComponent
+    },
+    {
+      path: 'personatalleres',
+      component: PersonaTallerComponent
+    },
+    {
+      path: 'reporte',
+      component: ReporteComponent
+    }
+  ]
   }
 ];
 @NgModule({
